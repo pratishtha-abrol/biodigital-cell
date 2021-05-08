7053bc7acc52f08d44043a179a4b9196303209aa
Pratishtha Abrol <pratishthaabrol@gmail.com>
2021-05-08 14:30:14 +0530

    update

--- js/animal_cell.js
+++ js/animal_cell.js
@@ -1,110 +1,90 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
-import {OBJLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/OBJLoader.js';
-import {MTLLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/MTLLoader.js';
+import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
 
 function main() {
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({canvas});
-  
+
     const fov = 45;
     const aspect = 2;  // the canvas default
     const near = 0.1;
     const far = 100;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 10, 20);
-  
+    camera.position.set(0, 10, 10);
+
     const controls = new OrbitControls(camera, canvas);
-    controls.target.set(0, 5, 0);
+    controls.target.set(0, 0, 0);
     controls.update();
-  
+
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color('white');
-  
+    scene.background = new THREE.Color('gray');
+
     {
-      const skyColor = 0xB1E1FF;  // light blue
-      const groundColor = 0xB97A20;  // brownish orange
-      const intensity = 1;
-      const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-      scene.add(light);
+        const skyColor = 0xFFFFFF;  // light blue
+        const groundColor = 0xFFFFFF;  // brownish orange
+        const intensity = 1;
+        const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        scene.add(light);
     }
-  
-    {
-      const color = 0xFFFFFF;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(5, 10, 2);
-      scene.add(light);
-      scene.add(light.target);
+
+    function addlight(...pos) {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(...pos);
+        scene.add(light);
+        scene.add(light.target);
     }
-  
-    function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
-      const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
-      const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
-      const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
-      const direction = (new THREE.Vector3())
-          .subVectors(camera.position, boxCenter)
-          .multiply(new THREE.Vector3(1, 0, 1))
-          .normalize();
-  
-      camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
-  
-      camera.near = boxSize / 100;
-      camera.far = boxSize * 100;
-  
-      camera.updateProjectionMatrix();
-  
-      camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
-    }
-  
-    {
-      const mtlLoader = new MTLLoader();
-      mtlLoader.load('assets/animal_cell/animalcellCOLOR.mtl', (mtl) => {
-        mtl.preload();
-        const objLoader = new OBJLoader();
-        objLoader.setMaterials(mtl);
-        objLoader.load('assets/animal_cell/animalcellCOLOR.obj', (root) => {
-          scene.add(root);
-  
-          const box = new THREE.Box3().setFromObject(root);
-  
-          const boxSize = box.getSize(new THREE.Vector3()).length();
-          const boxCenter = box.getCenter(new THREE.Vector3());
-  
-          frameArea(boxSize * 1.2, boxSize, boxCenter, camera);
-  
-          controls.maxDistance = boxSize * 10;
-          controls.target.copy(boxCenter);
-          controls.update();
+    addlight(10, 10, 10);
+    addlight(10, -10, 10);
+
+    function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+        const localPrefix = isLast ? '└─' : '├─';
+        lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+        const newPrefix = prefix + (isLast ? '  ' : '│ ');
+        const lastNdx = obj.children.length - 1;
+        obj.children.forEach((child, ndx) => {
+          const isLast = ndx === lastNdx;
+          dumpObject(child, lines, isLast, newPrefix);
         });
-      });
+        return lines;
+      }
+
+    {
+        const gltfLoader = new GLTFLoader();
+        gltfLoader.load('assets/animal_cell/scene.gltf', (gltf) => {
+            const root = gltf.scene;
+            scene.add(root);
+            console.log(dumpObject(root).join('\n'));
+        });
     }
-  
+
     function resizeRendererToDisplaySize(renderer) {
-      const canvas = renderer.domElement;
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
-      const needResize = canvas.width !== width || canvas.height !== height;
-      if (needResize) {
-        renderer.setSize(width, height, false);
-      }
-      return needResize;
-    }
-  
-    function render() {
-  
-      if (resizeRendererToDisplaySize(renderer)) {
         const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-      }
-  
-      renderer.render(scene, camera);
-  
-      requestAnimationFrame(render);
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+        return needResize;
     }
-  
+    
+    function render() {
+        // console.log(camera.position);
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+    
+        renderer.render(scene, camera);
+    
+        requestAnimationFrame(render);
+    }
+    
     requestAnimationFrame(render);
-  }
+}
 
 main();
